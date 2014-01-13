196d7a4b14acbc4ec76cec41d6b2f6cd65022da8
Maxime Arthaud <maxime.arthaud@gmail.com>
2014-01-13 13:50:11 +0100

    backend: connection to database

--- src/backend/Worker.java
+++ src/backend/Worker.java
@@ -1,4 +1,5 @@
 package backend;
+import java.sql.*;
 import java.util.logging.Logger;
 import org.apache.commons.cli.ParseException;
 
@@ -6,6 +7,7 @@ public class Worker
 {
     private Logger log;
     private WorkerConfig config;
+    private Connection con;
 
     public static void main(String argv[])
     {
@@ -19,20 +21,46 @@ public class Worker
                 worker.run();
             }
         }
-        catch(ParseException e)
-        {
+        catch(ParseException e) {
+            System.out.println(e);
+        }
+        catch(SQLException e) {
             System.out.println(e);
         }
     }
 
-    public Worker(WorkerConfig config)
+    public Worker(WorkerConfig config) throws SQLException
     {
         this.log = Logger.getLogger("worker");
         this.config = config;
+
+        // connection to database
+        con = DriverManager.getConnection(config.getDatabaseUrl(), config.getDatabaseLogin(), config.getDatabasePassword());
     }
 
-    public void run()
+    public void run() throws SQLException
     {
         log.info("Launching worker...");
+
+        try
+        {
+            ResultSet rs = con.createStatement().executeQuery("SELECT * FROM test");
+
+            while(rs.next())
+            {
+                for(int i=1; i <= rs.getMetaData().getColumnCount(); i++)
+                {
+                    System.out.println("column " + i + " = " + rs.getObject(i));
+                }
+            }
+        }
+        finally {
+            try {
+                con.close();
+            }
+            catch(Throwable ignore) {}
+        }
+
+        log.info("Exiting...");
     }
 }
